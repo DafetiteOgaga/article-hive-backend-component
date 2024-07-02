cfd56da186e7dffa986b52b49a602bd636d68446
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-07-02 20:18:45 +0100

    changes: added routes for other pages

--- article_hive_project/article_hive_app/urls.py
+++ article_hive_project/article_hive_app/urls.py
@@ -4,6 +4,13 @@ from . import views
 urlpatterns = [
 	# Create your urlpatterns here.
 	path('', views.home, name='home'),
+	path('hive/', views.hive, name='hive'),
+	path('article/', views.article, name='article'),
+	path('about/', views.about_page, name='about'),
+	path('contact/', views.contact_page, name='contact'),
+	path('profile/', views.profile_page, name='profile'),
+	path('login/', views.login_page, name='login'),
+	path('register/', views.register_page, name='register'),
 
 
 	path('ports/', views.checkRequest, name='checkRequest'),
