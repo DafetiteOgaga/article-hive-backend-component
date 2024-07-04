efc39b4f069b604a211adcf2c1146d5bd045269d
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-07-04 14:03:14 +0100

    changes: added and fixed article, comment, user and contact models

--- article_hive_project/article_hive_app/models.py
+++ article_hive_project/article_hive_app/models.py
@@ -6,35 +6,34 @@ from django.conf import settings # use settings.AUTH_USER_MODEL as User
 class User(AbstractUser):
 	middle_name = models.CharField(max_length=100, null=True, blank=True)
 	email = models.EmailField(max_length=200, unique=True)
+	# username = None
 	phone = models.SmallIntegerField(null=True, blank=True)
 	website = models.URLField(max_length=200, null=True, blank=True)
-	joined_since = models.DateTimeField(auto_now_add=True)
-	# number_of_articles = models.('Article', on_delete=models.CASCADE)
 	# profile_picture = models.ImageField()
 
 	USERNAME_FIELD = 'email'
-	REQUIRED_FIELDS = ['username']
+	REQUIRED_FIELDS = []
 	def __str__(self):
-		return self.username
+		return self.email
 
 class Article(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    content = models.TextField()
-    date_published = models.DateTimeField(auto_now_add=True)
-    # image = models.ImageField(upload_to='images/', required=False)
-    views = models.IntegerField(default=0)
-    comments = models.ForeignKey('Comment', on_delete=models.CASCADE)
-    rating = models.IntegerField(default=0)
+	title = models.CharField(max_length=200)
+	author = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	content = models.TextField()
+	date_published = models.DateTimeField(auto_now_add=True)
+	# image = models.ImageField(upload_to='images/', required=False)
+	views = models.IntegerField(default=0)
+	comments = models.ForeignKey('Comment', on_delete=models.CASCADE)
+	rating = models.IntegerField(default=0)
 
 class Comment(models.Model):
-    comment = models.TextField(max_length=500)
-    user = models.CharField(max_length=200)
-    # the_article = models.ForeignKey('Article', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now=True) # last modified stamp
+	comment = models.TextField(max_length=500)
+	user = models.CharField(max_length=200)
+	# the_article = models.ForeignKey('Article', on_delete=models.CASCADE)
+	created_at = models.DateTimeField(auto_now=True) # last modified stamp
 
 class Contact(models.Model):
-    contact = models.TextField(max_length=500)
-    name = models.CharField(max_length=200)
-    # the_article = models.ForeignKey('Article', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True) # creation stamp
+	contact = models.TextField(max_length=500)
+	name = models.CharField(max_length=200)
+	# the_article = models.ForeignKey('Article', on_delete=models.CASCADE)
+	created_at = models.DateTimeField(auto_now_add=True) # creation stamp
