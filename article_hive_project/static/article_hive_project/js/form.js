a5c6979c1535745b9555e93a05dbeebb65d02f74
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-07-06 01:53:57 +0100

    changes: added support for other form handling operations

--- article_hive_project/static/article_hive_project/js/form.js
+++ article_hive_project/static/article_hive_project/js/form.js
@@ -1,7 +1,52 @@
 const form = document.getElementById('form');
+const password = document.getElementById('password1');
+const password2 = document.getElementById('password2');
+const passwordMatchMessage = document.getElementById('password-match-message');
+const submitButton = document.getElementById('submit-button');
 form.addEventListener('submit', submitHandler);
+password.addEventListener('input', checkPasswordMatch);
+password2.addEventListener('input', checkPasswordMatch);
 
-let msg = ''
+// let msg = ''
+
+function checkPasswordMatch() {
+	// submitButton.disabled = true;
+	// passwordMatchMessage.style.fontStyle = 'italic';
+	// passwordMatchMessage.style.color = 'red';
+	passwordMatchMessage.textContent = '';
+	// passwordMatchMessage.style.height = '0';
+	// passwordMatchMessage.style.overflow = 'hidden';
+    if (password.value === '' && password2.value === '') {
+        passwordMatchMessage.textContent = '';
+        // passwordMatchMessage.style.height = '0';
+        // passwordMatchMessage.style.overflow = 'hidden';
+		// submitButton.disabled = true;
+    } else {
+        if (password.value === password2.value) {
+			if (password2.value.length < 8) {
+				passwordMatchMessage.textContent = 'Password must be atleast 8 characters.';
+				// passwordMatchMessage.style.color = 'red';
+				// submitButton.disabled = true;
+				// passwordMatchMessage.style.fontStyle = 'italic';
+				// passwordMatchMessage.style.color = 'red';
+			} else {
+				passwordMatchMessage.textContent = 'Passwords match.';
+				passwordMatchMessage.style.color = 'green';
+				// passwordMatchMessage.style.fontStyle = 'italic';
+				// submitButton.disabled = false;
+			}
+        } else {
+            passwordMatchMessage.textContent = 'Passwords do not match.';
+            // passwordMatchMessage.style.color = 'red';
+			// submitButton.disabled = true;
+			// passwordMatchMessage.style.fontStyle = 'italic';
+			// passwordMatchMessage.style.color = 'red';
+        }
+        passwordMatchMessage.style.height = 'auto';
+        passwordMatchMessage.style.overflow = 'visible';
+    }
+}
+
 function submitHandler (e) {
 	e.preventDefault();
 	// console.log('event activated')
@@ -9,20 +54,30 @@ function submitHandler (e) {
 
     const formData = new FormData(form);
 	const name = formData.get('name');
-	const firstname = formData.get('firstname');
+	const first_name = formData.get('first_name');
 	const email = formData.get('email');
 	const username = formData.get('username');
+	let msg = ''
 
     for (let [key, value] of formData.entries()) {
         console.log(`${key}: ${value}`);
 		if (key == 'comment') {
-			msg = `Dear ${firstname}, your comment has been updated and the author will be duly informed.\nThank you.`;
+			msg = `Dear ${first_name}, your comment has been updated and the author will be duly informed.\nThank you.`;
 			break;
 		} else if (key == 'contact') {
 			msg = `Thank You ${name} for reaching out to us!\nYour comment is currently under review and we promise to take it with utmost importance.\nWe value your input and we will keep working to improve your experience.`;
+			// form.reset();
 			break;
-		} else if (key == 'number') {
-			msg = `Registration successful!\nUsername: ${firstname}\nUsername: ${email}`;
+		} else if (key == 'phone') {
+			const password = formData.get('password1');
+			const password2 = formData.get('password2');
+			if (password !== password2) {
+				console.log('Passwords do not match')
+                alert('Passwords do not match.');
+                return;
+			}
+			console.log('Passwords ok')
+			msg = `Registration successful!\nUsername: ${first_name}\nUsername: ${email}`;
 			break;
 		} else {
 			msg = `Welcome ${username}`;
@@ -50,10 +105,21 @@ function submitHandler (e) {
 			console.log('###################');
 			if (data.message === 'success') {
 				alert(msg);
+				form.reset()
 			} else if (data.message === 'error') {
 				alert('Oopsy! Something went wrong.\nCheck your details again.');
 				window.location.reload();
-            }
+            } else if (data.message === 'incorrect password') {
+				alert('Your passwords does not match.\nCheck your details and try again.');
+				window.location.reload();
+            } else if (data.message === 'form invalid') {
+				let errorMessage = '';
+				for (const [key, value] of Object.entries(data.errors)) {
+					errorMessage += `${key}: ${value}\n`;
+				}
+				alert(errorMessage);
+				window.location.reload();
+			}
 			// form.reset()
 		}
 	})
