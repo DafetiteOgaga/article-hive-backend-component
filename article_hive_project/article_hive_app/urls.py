1b4f85d60c677b1ccbbcef484f82b1805455cddb
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-07-06 01:50:09 +0100

    changes: routes setup

--- article_hive_project/article_hive_app/urls.py
+++ article_hive_project/article_hive_app/urls.py
@@ -5,14 +5,23 @@ urlpatterns = [
 	# Create your urlpatterns here.
 	path('', views.home, name='home'),
 	path('hive/', views.hive, name='hive'),
-	path('article/', views.article, name='article'),
+	path('article/<int:pk>/', views.article, name='article'),
 	path('about/', views.about_page, name='about'),
 	path('contact/', views.contact_page, name='contact'),
-	path('profile/', views.profile_page, name='profile'),
+	path('profile/<int:pk>/', views.profile_page, name='profile'),
+	path('profile-update/<int:pk>/', views.profile_update, name='profile_update'),
 	path('login/', views.login_page, name='login'),
 	path('logout/', views.logout_page, name='logout'),
 	path('register/', views.register_page, name='register'),
 
+
+	# path('article/new/<int:pk>/', views.article_form, name='article_form'),
+	path('article/new/', views.article_form, name='article_form'),
+	path('update-article-form/<int:pk>/', views.update_article_form, name='update_article_form'),
+
+
+	# path('', views.article_list, name='article_list'),
+    # path('article/<int:pk>/', views.article_detail, name='article_detail'),
 
 
 	path('test_authentication/', views.test_authentication, name='test_authentication'),
