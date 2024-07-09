506049bcfda0c593fb8e0e9e597b8b6975e73d80
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-07-09 07:48:03 +0100

    changes: configured MySQL database to persist data, emailbackend to send emails to users for account recovery and memedia root, url in prep for production

--- article_hive_project/article_hive_project/settings.py
+++ article_hive_project/article_hive_project/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import configparser
+import os
+
+config = configparser.ConfigParser()
+config.read('../emailBackend_config.cnf')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,9 +30,10 @@ SECRET_KEY = 'django-insecure-ks%dws4&%hgxe21a(9g+8dmp)9l(tq7k&w&s4cuz2=dy4joql^
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = []
 
+# DEBUG = False # tests
+# ALLOWED_HOSTS = ['*'] # tests
 
 # Application definition
 
@@ -75,10 +82,19 @@ WSGI_APPLICATION = 'article_hive_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '../MySQL_credentials.cnf',
+        },
     }
 }
 
@@ -135,3 +151,19 @@ AUTH_USER_MODEL = 'article_hive_app.User'
 
 # login redirect for anonymous users
 LOGIN_URL = '/login/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config.get('email', 'host')
+EMAIL_PORT = config.getint('email', 'port')
+EMAIL_HOST_USER = config.get('email', 'user')
+EMAIL_HOST_PASSWORD = config.get('email', 'password')
+EMAIL_USE_TLS = config.getboolean('email', 'use_tls')
+DEFAULT_FROM_EMAIL = config.get('email', 'default_from')
+
+# # urls for password reset
+# LOGIN_REDIRECT_URL = 'home'
+# LOGOUT_REDIRECT_URL = 'home'
+
+# Media files custom added
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
