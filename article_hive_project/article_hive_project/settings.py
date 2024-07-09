bbfdfa424fec23467ff0626c1f47955db9ed76d1
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-07-09 12:12:59 +0100

    deployment update

--- article_hive_project/article_hive_project/settings.py
+++ article_hive_project/article_hive_project/settings.py
@@ -29,11 +29,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ks%dws4&%hgxe21a(9g+8dmp)9l(tq7k&w&s4cuz2=dy4joql^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = []
+# DEBUG = True
+# ALLOWED_HOSTS = []
 
-# DEBUG = False # tests
-# ALLOWED_HOSTS = ['*'] # tests
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+# DEBUG = False # production
+ALLOWED_HOSTS = [
+    'article-hive.vercel.app',
+    'localhost',
+    '127.0.0.1'
+]
+VERCEL_URL = os.environ.get('VERCEL_URL')
+if VERCEL_URL:
+    ALLOWED_HOSTS.append(VERCEL_URL)
 
 # Application definition
 
@@ -82,21 +90,21 @@ WSGI_APPLICATION = 'article_hive_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': '../MySQL_credentials.cnf',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'OPTIONS': {
+#             'read_default_file': '../MySQL_credentials.cnf',
+#         },
+#     }
+# }
 
 
 # Password validation
@@ -153,12 +161,18 @@ AUTH_USER_MODEL = 'article_hive_app.User'
 LOGIN_URL = '/login/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = config.get('email', 'host')
-EMAIL_PORT = config.getint('email', 'port')
-EMAIL_HOST_USER = config.get('email', 'user')
-EMAIL_HOST_PASSWORD = config.get('email', 'password')
-EMAIL_USE_TLS = config.getboolean('email', 'use_tls')
-DEFAULT_FROM_EMAIL = config.get('email', 'default_from')
+EMAIL_HOST = os.environ.get('email_host')
+EMAIL_PORT = os.environ.get('email_port')
+EMAIL_HOST_USER = os.environ.get('email_user')
+EMAIL_HOST_PASSWORD = os.environ.get('email_password')
+DEFAULT_FROM_EMAIL = os.environ.get('email_default_from')
+EMAIL_USE_TLS = True
+# EMAIL_HOST = config.get('email', 'host')
+# EMAIL_PORT = config.getint('email', 'port')
+# EMAIL_HOST_USER = config.get('email', 'user')
+# EMAIL_HOST_PASSWORD = config.get('email', 'password')
+# EMAIL_USE_TLS = config.getboolean('email', 'use_tls')
+# DEFAULT_FROM_EMAIL = config.get('email', 'default_from')
 
 # # urls for password reset
 # LOGIN_REDIRECT_URL = 'home'
@@ -167,3 +181,15 @@ DEFAULT_FROM_EMAIL = config.get('email', 'default_from')
 # Media files custom added
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# for production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+##########
+# STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'  # For collectstatic
+# STATICFILES_DIRS = [BASE_DIR / 'static']
+
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = BASE_DIR / 'media'
+##########
