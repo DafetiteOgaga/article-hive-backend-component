96fce7548a84ebe092aae9be21d9a9bd05ed7488
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-07-13 11:52:47 +0100

    new: handles the footer's date and time display

---
+++ article_hive_project/staticfiles/article_hive_project/js/date-time.js
@@ -0,0 +1,15 @@
+const monthNames = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
+const dayNames = [
+"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+];
+const now = new Date();
+const dayOfWeek = dayNames[now.getDay()];
+const month = monthNames[now.getMonth()];
+const dayOfMonth = now.getDate();
+const year = now.getFullYear();
+
+date_time = document.getElementById('date-time');
+date_time.innerHTML = `&copy; The Article Hive • ${dayOfWeek.slice(0, 3)}, ${month} ${dayOfMonth}, ${year}`;
