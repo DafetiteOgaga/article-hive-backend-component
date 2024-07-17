2f8b5a0624d7f0fa13c339f3d8ad72ab8e0d6129
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-07-17 05:10:44 +0100

    new: collected vanilla javascript for autocomplete during searches

---
+++ article_hive_project/staticfiles/article_hive_project/js/search.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+	const searchInput = document.getElementById('search-input');
+	
+	searchInput.addEventListener('input', function() {
+	  const query = this.value;
+	  if (query.length > 2) {
+		fetch(`/autocomplete/?q=${query}`)
+		  .then(response => response.json())
+		  .then(data => {
+			// Update your UI with autocomplete suggestions
+			console.log(data);
+		  });
+	  }
+	});
+  });
