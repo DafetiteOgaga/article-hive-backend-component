67eedd943209cc6bfbf97602c1c5d7f30941ebf8
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-07-18 08:51:19 +0100

    changes: changed the route for password reset to point to the new FBV logic

--- article_hive_project/article_hive_app/urls.py
+++ article_hive_project/article_hive_app/urls.py
@@ -23,7 +23,8 @@ urlpatterns = [
 
 	path('password_change/', views.CustomPasswordChangeView.as_view(), name='password_change'),
     path('password_change/done/', views.CustomPasswordChangeDoneView.as_view(), name='password_change_done'),
-    path('password_reset/', views.CustomPasswordResetView.as_view(), name='password_reset'),
+    # path('password_reset/', views.CustomPasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/', views.custom_password_reset, name='password_reset'),
     path('password_reset/done/', views.CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', views.CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', views.CustomPasswordResetCompleteView.as_view(), name='password_reset_complete'),
