6ad0ec94a9f3adfae0a38a4145bec2b98b2a70ef
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-07-21 03:23:33 +0100

    fix: handled encoded image and and embedded it into the mail content for better UI and UX

--- article_hive_project/article_hive_app/send_mail.py
+++ article_hive_project/article_hive_app/send_mail.py
@@ -1,15 +1,10 @@
 import os, sys, requests, base64 #, json
 from django.template.loader import render_to_string
 from django.urls import reverse
-# from .logo import article_hive_logo
 
 sys.path.append(os.path.expanduser("~"))
 from mhykhehy import api_key
-# print(f"my key: {api_key}")
 url = 'https://api.brevo.com/v3/smtp/email'
-
-# img = "static/article_hive_project/logo - shade of brown/the-article-hive-high-resolution-logo-transparent.png"
-# print('is img correct?', os.path.isfile(img))
 
 with open("static/article_hive_project/logo - shade of brown/the-article-hive-high-resolution-logo-transparent.png", "rb") as image_file:
 		encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
@@ -24,15 +19,8 @@ def send_password_reset_email(request: object, user: object, reset_link: str):
 		'user': user.first_name,
 		'reset_link': reset_link,
 		'article_support_url': req(request=request),
-		# 'article_hive_logo': article_hive_logo,
 		'heading': 'Password Reset Request'
 	})
-
-	# print(f"user: {user}")
-	# print(f"email: {user.email}")
-	# print(f"firstname: {user.first_name}")
-	# print(f"subject: {subject}")
-	# print(f"reset link: {reset_link}")
 
 	email_data = {
 		'sender': {'name': 'Article-Hive', 'email': 'ogagadafetite@gmail.com'},
@@ -51,9 +39,9 @@ def send_password_reset_email(request: object, user: object, reset_link: str):
 
 	# Request headers
 	headers = {
-		'accept': 'application/json',  # Accept JSON response
-		'api-key': api_key,  # API key header
-		'content-type': 'application/json'  # Content type header
+		'accept': 'application/json',
+		'api-key': api_key,
+		'content-type': 'application/json'
 	}
 
 	response = requests.post(url, headers=headers, json=email_data) # , data=json.dumps(email_data))
@@ -66,16 +54,8 @@ def welcome_email(request: object, user: object, article_url: str):
 		'user': user.first_name,
 		'article_url': article_url,
 		'article_support_url': req(request=request),
-		# 'article_hive_logo': article_hive_logo,
 		'heading': 'Welcome to Article Hive'
 	})
-
-	# print(f"user: {user}")
-	# print(f"email: {user.email}")
-	# print(f"firstname: {user.first_name}")
-	# print(f"article_url: {article_url}")
-	# print(f"subject: {subject}")
-	# # print(f"reset link: {reset_link}")
 
 	email_data = {
 		'sender': {'name': 'Article-Hive', 'email': 'ogagadafetite@gmail.com'},
@@ -109,23 +89,8 @@ def comment_notification_email(request: object, comment_author: str, comment: ob
 		'comment': comment,
 		'article_url': article_url,
 		'article_support_url': req(request=request),
-		# 'article_hive_logo': article_hive_logo,
 		'heading': 'New Comment on Your Article'
-		# 'heading': 'Comment to your Article'
 	})
-
-	# print(f"##################### comment #####################")
-	# print(f"user: {comment.article.author}")
-	# print(f"title: {comment.article.title}")
-	# print(f"email: {comment.article.author.email}")
-	# print(f"comment_author: {comment_author}")
-	# print(f"comment: {comment}")
-	# print(f"article: {comment.article}")
-	# print(f"article_url: {article_url}")
-	# print(f"firstname: {comment.article.author.first_name}")
-	# print(f"subject: {subject}")
-	# # print(f"reset link: {reset_link}")
-	# print(f"#####################  #####################")
 
 	email_data = {
 		'sender': {'name': 'Article-Hive', 'email': 'ogagadafetite@gmail.com'},
@@ -158,24 +123,8 @@ def author_reply_notification_email(request: object, reply: object, article_url:
 		'reply': reply,
 		'article_url': article_url,
 		'article_support_url': req(request=request),
-		# 'article_hive_logo': article_hive_logo,
 		'heading': f'Response to your comment on {reply.comment.article.title}'
-		# 'heading': 'Comment to your Article'
 	})
-
-	# print(f"##################### reply #####################")
-	# print(f"user: {reply.comment.article.author}")
-	# print(f"title: {reply.comment.article.title}")
-	# print(f"email: {reply.comment.article.author.email}")
-	# print(f"comment_author: {reply.comment.user.first_name}")
-	# print(f"commentor_email: {reply.comment.user.email}")
-	# print(f"reply: {reply}")
-	# print(f"article: {reply.comment.article}")
-	# print(f"article_url: {article_url}")
-	# print(f"firstname: {reply.comment.article.author.first_name}")
-	# print(f"subject: {subject}")
-	# # print(f"reset link: {reset_link}")
-	# print(f"#####################  #####################")
 
 	email_data = {
 		'sender': {'name': 'Article-Hive', 'email': 'ogagadafetite@gmail.com'},
@@ -206,20 +155,9 @@ def password_reset_complete_email(request: object, user: object):
 	subject = render_to_string('emails/password_reset_complete_subject.txt', {'user': user}).strip()
 	email_body = render_to_string('emails/password_reset_complete_email.html', {
 		'user': user.first_name,
-		# 'article_url': article_url,
 		'article_support_url': req(request=request),
-		# 'article_hive_logo': article_hive_logo,
 		'heading': f'Password Reset Successful',
-		# 'heading': 'Comment to your Article'
 	})
-
-	# print(f"##################### reset #####################")
-	# print(f"user: {user}")
-	# print(f"email: {user.email}")
-	# print(f"first name: {user.first_name}")
-	# print(f"subject: {subject}")
-	# # print(f"reset link: {reset_link}")
-	# print(f"#####################  #####################")
 
 	email_data = {
 		'sender': {'name': 'Article-Hive', 'email': 'ogagadafetite@gmail.com'},
@@ -249,25 +187,11 @@ def password_reset_complete_email(request: object, user: object):
 def password_change_done_email(request: object, user: object):
 	subject = render_to_string('emails/password_change_done_subject.txt', {'user': user}).strip()
 
-	# with open("static/article_hive_project/logo - shade of brown/the-article-hive-high-resolution-logo-transparent.png", "rb") as image_file:
-	# 	encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
-
 	email_body = render_to_string('emails/password_change_done_email.html', {
 		'user': user.first_name,
-		# 'article_url': article_url,
 		'article_support_url': req(request=request),
-		# 'article_hive_logo': article_hive_logo,
 		'heading': f'Password Change Successful',
-		# 'heading': 'Comment to your Article'
 	})
-
-	# print(f"##################### change #####################")
-	# print(f"user: {user}")
-	# print(f"email: {user.email}")
-	# print(f"first name: {user.first_name}")
-	# print(f"subject: {subject}")
-	# # print(f"reset link: {reset_link}")
-	# print(f"#####################  #####################")
 
 	email_data = {
 		'sender': {'name': 'Article-Hive', 'email': 'ogagadafetite@gmail.com'},
@@ -292,8 +216,6 @@ def password_change_done_email(request: object, user: object):
 	}
 
 	response = requests.post(url, headers=headers, json=email_data) # data=json.dumps(email_data))
-	# print(f"Response status: {response.status_code}")
-	# print(f"Response content: {response.text}")
 	return response
 
 def congratulations_first_post(request: object, user: object, article_url: str):
@@ -302,18 +224,8 @@ def congratulations_first_post(request: object, user: object, article_url: str):
 		'user': user.first_name,
 		'article_url': article_url,
 		'article_support_url': req(request=request),
-		# 'article_hive_logo': article_hive_logo,
 		'heading': f'Congratulations on your First Post',
-		# 'heading': 'Comment to your Article'
 	})
-
-	# print(f"##################### congratulations #####################")
-	# print(f"user: {user}")
-	# print(f"email: {user.email}")
-	# print(f"first name: {user.first_name}")
-	# print(f"subject: {subject}")
-	# # print(f"reset link: {reset_link}")
-	# print(f"#####################  #####################")
 
 	email_data = {
 		'sender': {'name': 'Article-Hive', 'email': 'ogagadafetite@gmail.com'},
