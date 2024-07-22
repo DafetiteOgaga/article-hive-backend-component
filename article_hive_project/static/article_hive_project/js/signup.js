85fb003874d10a7e4bc795a5f54a6b2fa87e85e1
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-07-22 23:51:19 +0100

    add: wrapped the code in a try-catch block to prevent error display on browser console

--- article_hive_project/static/article_hive_project/js/signup.js
+++ article_hive_project/static/article_hive_project/js/signup.js
@@ -1,33 +1,33 @@
-// JavaScript file: js/scripts.js text
-
-document.addEventListener('DOMContentLoaded', function() {
-    const textElement = document.querySelector('.signup');
-    const text1 = "Sign-up NOW to start making Posts!";
-    const text2 = "Get email notifications for posts, comments, replies and more!";
-    let current = true;
-    const getText = () => {
-        return current ? text1 : text2;
-    }
-    let index = 0;
-    const typingSpeed = 100; //speed
-
-    function typeText() {
-        const text = getText()
-        if (index < text.length) {
-            textElement.textContent += text.charAt(index);
-            index++;
-            setTimeout(typeText, typingSpeed);
-        } else {
-            setTimeout(resetAndTypeText, 2000);  // Pauses the cursor before restarting
+try {
+    document.addEventListener('DOMContentLoaded', function() {
+        const textElement = document.querySelector('.signup');
+        const text1 = "Sign-up NOW to start making Posts!";
+        const text2 = "Get email notifications for posts, comments, replies and more!";
+        let current = true;
+        const getText = () => {
+            return current ? text1 : text2;
         }
-    }
+        let index = 0;
+        const typingSpeed = 100; //speed
 
-    function resetAndTypeText() {
-        textElement.textContent = '';
-        index = 0;
-        current = !current
+        function typeText() {
+            const text = getText()
+            if (index < text.length) {
+                textElement.textContent += text.charAt(index);
+                index++;
+                setTimeout(typeText, typingSpeed);
+            } else {
+                setTimeout(resetAndTypeText, 2000);  // Pauses the cursor before restarting
+            }
+        }
+
+        function resetAndTypeText() {
+            textElement.textContent = '';
+            index = 0;
+            current = !current
+            typeText();
+        }
+
         typeText();
-    }
-
-    typeText();
-});
+    });
+} catch (e) {}
