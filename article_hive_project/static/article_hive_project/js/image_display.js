c3442ed1b0ceaa6f69ed86f8260c14d597e6f418
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-07-22 23:51:14 +0100

    add: wrapped the code in a try-catch block to prevent error display on browser console

--- article_hive_project/static/article_hive_project/js/image_display.js
+++ article_hive_project/static/article_hive_project/js/image_display.js
@@ -1,13 +1,15 @@
-document.getElementById('profile_picture').addEventListener('change', function(event) {
-    var reader = new FileReader();
-    reader.onload = function(){
-        var output = document.getElementById('selectedImage');
-        output.src = reader.result;
-        output.style.display = 'block';
-    };
-    if (event.target.files[0]) {
-        reader.readAsDataURL(event.target.files[0]);
-    } else {
-        document.getElementById('selectedImage').style.display = 'none';
-    }
-});
+try {
+    document.getElementById('profile_picture').addEventListener('change', function(event) {
+        var reader = new FileReader();
+        reader.onload = function(){
+            var output = document.getElementById('selectedImage');
+            output.src = reader.result;
+            output.style.display = 'block';
+        };
+        if (event.target.files[0]) {
+            reader.readAsDataURL(event.target.files[0]);
+        } else {
+            document.getElementById('selectedImage').style.display = 'none';
+        }
+    });
+} catch (e) {}
