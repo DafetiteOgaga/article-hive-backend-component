7e88f5c9b4eeef3b8c570f7b3ccbc4d6481d0413
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-07-22 23:59:55 +0100

    add: collected static js files

--- article_hive_project/staticfiles/js/search.js
+++ article_hive_project/staticfiles/js/search.js
@@ -1,15 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
-	const searchInput = document.getElementById('search-input');
-	
-	searchInput.addEventListener('input', function() {
-	  const query = this.value;
-	  if (query.length > 2) {
-		fetch(`/autocomplete/?q=${query}`)
-		  .then(response => response.json())
-		  .then(data => {
-			// Update UI with autocomplete suggestions
-			console.log(data);
-		  });
-	  }
+try {
+	document.addEventListener('DOMContentLoaded', function() {
+		const searchInput = document.getElementById('search-input');
+		
+		searchInput.addEventListener('input', function() {
+		const query = this.value;
+		if (query.length > 2) {
+			fetch(`/autocomplete/?q=${query}`)
+			.then(response => response.json())
+			.then(data => {
+				// Update your UI with autocomplete suggestions
+				console.log(data);
+			});
+		}
+		});
 	});
-  });
+} catch (e) {}
