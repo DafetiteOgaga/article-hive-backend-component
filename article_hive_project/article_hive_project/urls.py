e5567a5fada6f2d960dff081b0515b0465676af2
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-07-25 15:40:10 +0100

    changes: added route and CBV for cache template monitoring

--- article_hive_project/article_hive_project/urls.py
+++ article_hive_project/article_hive_project/urls.py
@@ -20,10 +20,14 @@ from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 # from .project_views import custom_error_view
+from django.views.decorators.cache import never_cache
+from django.views.generic import TemplateView
+from django.contrib.admin.views.decorators import staff_member_required
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('article_hive_app.urls')),     # For article_hive_app configuration.
+    path('cache-stats/', never_cache(staff_member_required(TemplateView.as_view(template_name='global_cache_stats.html'))), name='cache_stats'),
 ]
 
 if settings.DEBUG:
